65f0d9fcd32b855c49dc1ab5fad75fbd861eac36
platonvin <platonvin@gmail.com>
2023-08-02 00:31:34 +0300

    idk why didnt it commit theese files

--- client/draw/shaders/vertex.glsl
+++ client/draw/shaders/vertex.glsl
@@ -3,11 +3,16 @@
 layout(location = 0) in vec3 vertex_coords;
 layout(location = 1) in float tile_num;
 
-out vec2 coords;
-out vec2 uv;
+out vec2 screen_coords;
+out vec2 tileset_uv;
+
+mat3 transformation = 
+{
+    
+}
 
 void main(){
-    coords = vertex_coords;
-    UV = vec2(tile_num, 0)
+    screen_coords = vertex_coords;
+    tileset_uv = vec2(tile_num, 0)
     gl_Position = vec4(vertexCoords, 1.0);
 }
