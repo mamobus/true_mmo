4a787e6d3295d8c9d50fa32d4f2ccb0b0d1a3688
platonvin <platonvin@gmail.com>
2023-08-10 04:27:06 +0300

    swithced to loading all the map into ram

--- client/map/load.h
+++ client/map/load.h
@@ -1,15 +1,37 @@
 #pragma once
 
-// #include <GL/glew.h>
-#include "../common/game.h"
 #include <string.h>
-// #include "../draw/load_texture.h"
 #include "cute_tiled.h"
 #include <malloc.h>
+#include <stdint.h>
+
+typedef struct tile_t
+{
+    float x;
+    float y;
+    float z;
+    float tile_num;
+} tile_t;
+
+typedef struct chunk_ram_t
+{
+    //in bytes
+    int shift;
+    int tile_count;
+} chunk_ram_t;
+
+
+typedef struct map_t
+{
+    int width;
+    int height;
+
+    chunk_ram_t* chunks;
+    tile_t* tiles;
+} map_t; 
+
 
 char* get_tileset_path(char* map_path, char* relative_tileset_path);
 
-void load_level(chunk_system_t* chunk_system);
-
-void free_level(chunk_system_t* chunk_system);
+void load_map_from_file(char* map_path, map_t* map);
 
